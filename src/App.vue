6507e7328cfaad16065ce79a5638cb6536c0d00c
Anton <137686641+chikibambony137@users.noreply.github.com>
2025-07-31 14:36:40 +0300

    adapt html and css to BEM style

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,116 @@
 <script setup></script>
 
 <template>
-  <div class="root">
-    <div class="aside">
-      <div class="logo-div">
-        <img class="logo-img" src="./assets/logo.png" alt="logo.png" />
-        <h1>ABSOLUTE CINEMA</h1>
+  <div class="main">
+    <div class="menu">
+      <div class="logo-title menu__logo-title">
+        <img class="logo-title__logo" src="./assets/logo.png" alt="logo.png" />
+        <h1 class="logo-title__title">ABSOLUTE CINEMA</h1>
       </div>
 
-      <nav>
+      <div class="menu__nav">
         <ul>
-          <div
-            style="
-              border-top: 1px solid #d5d5d5;
-              border-bottom: 1px solid #d5d5d5;
-            "
-          >
+          <div class="menu__outline">
             <li><a href="#">Главное меню</a></li>
           </div>
 
-          <li><a href="#">Фильмы</a></li>
-          <li><a href="#">Билеты</a></li>
+          <li>
+            <div class="menu__films">
+              <a href="#">Фильмы</a>
+            </div>
+          </li>
 
-          <div
-            style="
-              border-top: 1px solid #d5d5d5;
-              border-bottom: 1px solid #d5d5d5;
-            "
-          >
+          <li>
+            <div class="menu__tickets">
+              <a href="#">Билеты</a>
+            </div>
+          </li>
+
+          <div class="menu__outline">
             <li>
-              <img
-                class="cinema-banner"
-                src="./assets/cinemaBanner.png"
-                alt="cinema-banner.png"
-              />
+              <div class="banner menu__banner">
+                <img
+                  class="banner__img"
+                  src="./assets/cinemaBanner.png"
+                  alt="banner.png"
+                />
+              </div>
             </li>
-            <li><a href="#">Настройки</a></li>
-            <li><a href="#">Помощь</a></li>
+
+            <li>
+              <div class="menu__settings">
+                <a href="#">Настройки</a>
+              </div>
+            </li>
+
+            <li>
+              <div class="menu__help">
+                <a href="#">Помощь</a>
+              </div>
+            </li>
           </div>
 
           <li>
-            <div class="user"><a href="#">Агасиев Саид</a></div>
+            <div class="user menu__user"><a href="#">Агасиев Саид</a></div>
           </li>
-          <li><button>Выйти</button></li>
+          <li>
+            <div class="log-out menu__log-out">
+              <button class="log-out__bttn">Выйти</button>
+            </div>
+          </li>
         </ul>
-      </nav>
+      </div>
     </div>
 
-    <div class="main">
+    <div class="content">
       <h2>Фильмы</h2>
 
-      <div class="genres">
+      <div class="genres content__genres">
         <ul>
           <li>
-            <label class="checkbox-btn">
+            <label class="genres__checkbox-btn">
               <input type="checkbox" />
               <span>Комедия</span>
             </label>
           </li>
           <li>
-            <label class="checkbox-btn">
+            <label class="genres__checkbox-btn">
               <input type="checkbox" />
               <span>Драма</span>
             </label>
           </li>
           <li>
-            <label class="checkbox-btn">
+            <label class="genres__checkbox-btn">
               <input type="checkbox" />
               <span>Фантастика</span>
             </label>
           </li>
           <li>
-            <label class="checkbox-btn">
+            <label class="genres__checkbox-btn">
               <input type="checkbox" />
               <span>Триллер</span>
             </label>
           </li>
           <li>
-            <label class="checkbox-btn">
+            <label class="genres__checkbox-btn">
               <input type="checkbox" />
               <span>Фэнтези</span>
             </label>
           </li>
           <li>
-            <label class="checkbox-btn">
+            <label class="genres__checkbox-btn">
               <input type="checkbox" />
               <span>Мюзикл</span>
             </label>
           </li>
           <li>
-            <label class="checkbox-btn">
+            <label class="genres__checkbox-btn">
               <input type="checkbox" />
               <span>Боевик</span>
             </label>
           </li>
           <li>
-            <label class="checkbox-btn">
+            <label class="genres__checkbox-btn">
               <input type="checkbox" />
               <span>Хоррор</span>
             </label>
@@ -103,23 +118,19 @@
         </ul>
       </div>
 
-      <div class="films">
-        <article class="film"></article>
-        <article class="film"></article>
-        <article class="film"></article>
-        <article class="film"></article>
-        <article class="film"></article>
-        <article class="film"></article>
-        <article class="film"></article>
-        <article class="film"></article>
-        <article class="film"></article>
-        <article class="film"></article>
-        <article class="film"></article>
-        <article class="film"></article>
-        <article class="film"></article>
-        <article class="film"></article>
-        <article class="film"></article>
-        <article class="film"></article>
+      <div class="films content_films">
+        <div class="films__film"></div>
+        <div class="films__film"></div>
+        <div class="films__film"></div>
+        <div class="films__film"></div>
+        <div class="films__film"></div>
+        <div class="films__film"></div>
+        <div class="films__film"></div>
+        <div class="films__film"></div>
+        <div class="films__film"></div>
+        <div class="films__film"></div>
+        <div class="films__film"></div>
+        <div class="films__film"></div>
       </div>
     </div>
   </div>
@@ -138,15 +149,59 @@ $dark-theme-text-color: white;
     top: 1px solid #d5d5d5;
     bottom: 1px solid #d5d5d5;
   }
+
   ul {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
-    justify-content:space-evenly;
+    justify-content: space-evenly;
     padding: 0;
     list-style-type: none;
     padding-left: 1vw;
     padding-right: 1.5vw;
+  }
+
+  .genres__checkbox-btn {
+    display: inline-block;
+    margin: 2%;
+    user-select: none;
+    position: relative;
+  }
+  .genres__checkbox-btn input[type="checkbox"] {
+    z-index: -1;
+    opacity: 0;
+    display: block;
+    width: 0;
+    height: 0;
+  }
+  .genres__checkbox-btn span {
+    display: inline-block;
+    cursor: pointer;
+    padding: 0px 10px;
+    line-height: 30px;
+    transition: background 0.2s ease;
+
+    box-shadow: $global-shadow-box;
+    border-radius: 20px;
+    font-size: 0.9rem;
+  }
+
+  /* Checked */
+  .genres__checkbox-btn input[type="checkbox"]:checked + span {
+    background: #fdc9ff;
+  }
+
+  /* Hover */
+  .genres__checkbox-btn:hover {
+    span {
+      background-color: rgb(246, 246, 246);
+    }
+  }
+
+  /* Active */
+  .genres__checkbox-btn input[type="checkbox"]:active:not(:disabled) + span {
+    background: #dedede;
+    color: #000;
   }
 }
 
@@ -169,7 +224,7 @@ $dark-theme-text-color: white;
   box-sizing: border-box;
   margin-top: 1dvh;
 
-  .film {
+  .films__film {
     display: flex;
 
     width: 100%;
@@ -190,70 +245,4 @@ $dark-theme-text-color: white;
     }
   }
 }
-
-.logo-div {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  gap: 0.5vw;
-
-  .logo-img {
-    width: 3rem;
-    height: 3rem;
-  }
-
-  h1 {
-    font-size: 1.2rem;
-    margin: 0px;
-  }
-}
-
-.cinema-banner {
-  width: 100%;
-}
-
-.checkbox-btn {
-  display: inline-block;
-  margin: 2%;
-  user-select: none;
-  position: relative;
-}
-.checkbox-btn input[type="checkbox"] {
-  z-index: -1;
-  opacity: 0;
-  display: block;
-  width: 0;
-  height: 0;
-}
-.checkbox-btn span {
-  display: inline-block;
-  cursor: pointer;
-  padding: 0px 10px;
-  line-height: 30px;
-  transition: background 0.2s ease;
-
-  box-shadow: $global-shadow-box;
-  border-radius: 20px;
-  font-size: 0.9rem;
-}
-
-/* Checked */
-.checkbox-btn input[type="checkbox"]:checked + span {
-  background: #FDC9FF;
-}
-
-/* Hover */
-.checkbox-btn:hover {
-  span {
-    background-color: rgb(246, 246, 246);
-  }
-}
-
-/* Active */
-.checkbox-btn input[type=checkbox]:active:not(:disabled) + span {
-	background: #dedede;
-	color: #000;
-}
-
 </style>
